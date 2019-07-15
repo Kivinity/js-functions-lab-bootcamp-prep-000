527894aab9bc25f91d619e762be0c2734c5047e3
Kathryn Stenberg <kate.stenberg@hotmail.com>
2019-07-15 19:30:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,5 @@ function happyHolidayTo(holiday, name) {
 function holidayCountdown(holiday, days) {
   return (`It's ${days} days until, ${holiday}!`)
 }
+
+{return ("It\'s 20 day until hear you!") }
